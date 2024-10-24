cd71635245c6d172fd28ddcc0e249942e16058ca
somnath <somnathmishra100dbi@gmail.com>
2024-10-24 18:05:46 +0530

    fix skeleton loading

--- src/components/SkeletonLoader.tsx
+++ src/components/SkeletonLoader.tsx
@@ -4,7 +4,7 @@ import { Squirrel } from 'lucide-react';
 export function SkeletonLoader() {
   return (
     <div className="animate-pulse cursor-wait">
-      <Skeleton className="col-span-1 aspect-[100/127] min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min flex items-center justify-center">
+      <Skeleton className="col-span-1 aspect-[100/127] flex-1 rounded-xl bg-muted/50 min-h-min flex items-center justify-center">
       <Squirrel className="size-10 animate-bounce"/>
       </Skeleton>
     </div>
